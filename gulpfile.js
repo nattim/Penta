096c21a0b328b6e3afab54ede0e017fc767ddd36
nattim <nattim2012@gmail.com>
2020-07-20 13:39:13 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+let gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync');
+
+gulp.task('scss', function(){
+ return gulp.src('app/scss/**/*.scss')
+	.pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+// gulp.task('sass', function () {
+//   gulp.src('app/scss/*.scss')
+//     .pipe(sass({
+//       // includePaths: require('node-normalize-scss').with('other/path', 'another/path')
+//       // - or -
+//       includePaths: require('node-normalize-scss').includePaths
+//     }))
+//     .pipe(gulp.dest('app/css'));
+// });
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+
+gulp.task('html', function(){
+ return gulp.src('app/*.html')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+	gulp.watch('app/*.html', gulp.parallel('html'))
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'))
